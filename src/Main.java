107e5a7d362f93cf6330b77743f95092a290226e
phamqui <darkscopio2@gmail.com>
2024-04-11 13:02:50 +0700

    here

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! game timekkkk ");
+        System.out.println("Hello world! game timekkkk hahahahahaah");
     }
 }
